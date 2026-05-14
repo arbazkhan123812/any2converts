abfaa90828fb0366904d0d11a33c9e58e2f6e8c5
Arbaz khan <arbaznadeem3130@gmail.com>
2026-05-14 11:09:17 +0500

    canoncial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -289,6 +289,7 @@ if (!empty($initialToolId) && isset($toolNameMap[$initialToolId])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="canonical" href="{{ url()->full() }}">
     <title><?= htmlspecialchars($seoTitle, ENT_QUOTES) ?></title>
     <link rel="icon" type="image/png" href="mylogo.png">
     <meta name="description" content="<?= htmlspecialchars($seoDescription, ENT_QUOTES) ?>">
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="canonical" href="{{ url()->full() }}">
     <title>{{ $title }}</title>
     <meta name="description" content="{{ $description ?? '' }}">
     <link rel="icon" type="image/png" href="/mylogo.png">
