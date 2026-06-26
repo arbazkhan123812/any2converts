6c9f17fd8d6559d9e6710802dc38dcfb4ead88e2
Arbaz khan <arbaznadeem3130@gmail.com>
2026-06-26 14:41:16 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1616,7 +1616,6 @@ if ($isToolPage) {
     ?>
 
     <?php
-    if (empty($initialToolId)):
     foreach ($tools as $catKey => $category):
         $cc = $catColors[$catKey];
     ?>
@@ -1639,6 +1638,7 @@ if ($isToolPage) {
                 $iconSvg = $iconSvgs[$tool['icon']] ?? $iconSvgs['pdf_to_word'];
             ?>
             <?php $tool_slug = $tool_slugs[$tool['id']] ?? $tool['id']; ?>
+            <!-- Note: href modified to use openTool locally to prevent redirection if JS is enabled -->
             <a
                 href="/<?= htmlspecialchars($tool_slug) ?>"
                 class="tool-card stagger-in"
@@ -1646,6 +1646,7 @@ if ($isToolPage) {
                 data-tool-name="<?= htmlspecialchars(strtolower($tool['name'])) ?>"
                 data-tool-desc="<?= htmlspecialchars(strtolower($tool['desc'])) ?>"
                 data-tool-category="<?= htmlspecialchars($catKey) ?>"
+                onclick="event.preventDefault(); openTool('<?= htmlspecialchars($tool['id']) ?>');"
                 style="animation-delay:<?= $i * 40 ?>ms; text-decoration:none; display:block;"
             >
                 <!-- Arrow -->
@@ -1667,7 +1668,6 @@ if ($isToolPage) {
     </section>
     <?php
     endforeach;
-    endif;
     ?>
 
     <!-- ── Why section ── -->
